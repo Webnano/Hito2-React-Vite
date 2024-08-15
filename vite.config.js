55e1dcf0b537c594ad7757fd693077820b544818
NanoMedina <medina.david.bravo@gmail.com>
2024-08-15 01:18:35 -0400

    levantamiento hito 2.3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/Webnano/Hito2-React-Vite",
+  base: "/Hito2-React-Vite",
   plugins: [react()],
 })
